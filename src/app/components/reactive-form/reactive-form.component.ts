2338ed2ed8b4218ead206cce86683f37a70f3c16
Stephen Healey <s_healey@hotmail.co.uk>
2021-01-17 14:04:07 +0000

    Multiple form-controls

--- src/app/components/reactive-form/reactive-form.component.ts
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -17,18 +17,48 @@ export class ReactiveFormComponent implements OnInit {
     this.form = new FormGroup({
       firstname: new FormControlModel({
         label: 'First Name',
-        placeholder: 'First Name',
         name: 'firstname',
+        placeholder: 'First Name',
+        icon: 'fas fa-user',
         validation: {
-          required: 'Please enter your firstname.',
-          minlength: 'Your first name must be at least 3 characters.',
-        },
-        icon: 'fas fa-user'
+          required: 'Please enter your first name.',
+          minlength: 'Your first name needs to be 3 or more characters.',
+        }
       }, '', [
         Validators.required,
         Validators.minLength(3)
+      ]),
+      lastname: new FormControlModel({
+        label: 'Last Name',
+        name: 'lastname',
+        placeholder: 'Last Name',
+        icon: 'fas fa-user',
+        validation: {
+          required: 'Please enter your last name.',
+          minlength: 'Your last name needs to be 3 or more characters.',
+        }
+      }, '', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      email: new FormControlModel({
+        label: 'Email',
+        name: 'email',
+        placeholder: 'Email',
+        icon: 'fas fa-envelope',
+        validation: {
+          required: 'Please enter your email.',
+          minlength: 'Your first email must be valid.',
+        }
+      }, '', [
+        Validators.required,
+        Validators.email
       ])
     });
+  }
+
+  public getControls(): Array<FormControlModel> {
+    return Object.values(this.form.controls) as Array<FormControlModel>;
   }
 
 }
